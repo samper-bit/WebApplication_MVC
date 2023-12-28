36b451918409c2f6ad5b4c1259cb669b7f0f8259
@ipz212_syav <ipz212_syav@student.ztu.edu.ua>
2023-12-28 15:29:59 +0200

    Git

---
+++ WebApplication_MVC/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication_MVC.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
